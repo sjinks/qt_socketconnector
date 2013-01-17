81c8b3023a96e336fc0400d1c61f98c85aad35fe
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-01-17 16:28:52 +0200

    Clean up

--- qtscriptgenerator/master.h
+++ qtscriptgenerator/master.h
@@ -1,6 +1,9 @@
 #ifndef MASTER_H
 #define MASTER_H
 
+#include <QtCore/QObject>
+#include <QtNetwork/QAbstractSocket>
+#include <QtNetwork/QHostAddress>
 #include "../src/socketconnector.h"
 
 #endif
